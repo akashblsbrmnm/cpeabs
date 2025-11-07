6aa0418a75e4961744dcde9ffb8db1426a6db448
Akash Balasubhramanyam <akashblsbrmnm@gmail.com>
2025-11-07 12:14:49 +0530

    fix get_deviceMAC for HAL Default MAC

--- src/rdkb/impl.c
+++ src/rdkb/impl.c
@@ -73,6 +73,7 @@
 #define MQTT_PORT_PARAM "Device.X_RDK_MQTT.Port"
 
 #endif
+#define DEFAULT_MAC    "001a2b112233"
 /*----------------------------------------------------------------------------*/
 /*                               Data Structures                              */
 /*----------------------------------------------------------------------------*/
@@ -187,7 +188,7 @@ char* get_deviceWanMAC()
 
 char* get_deviceMAC()
 {
-	if(strlen(deviceMAC) != 0)
+	if(strlen(deviceMAC) != 0 && strncmp(deviceMAC, DEFAULT_MAC, strlen(DEFAULT_MAC)) != 0)
 	{
 		CpeabsDebug("deviceMAC returned %s\n", deviceMAC);
 		return deviceMAC;
@@ -198,10 +199,10 @@ char* get_deviceMAC()
 	macID = getParamValue(DEVICE_MAC);
 	if (macID != NULL)
 	{
-	    cpeabStrncpy(deviceMACValue, macID, strlen(macID)+1);
-	    macIDToLower(deviceMACValue, deviceMAC);
-	    CpeabsDebug("deviceMAC: %s\n",deviceMAC);
-	    CPEABS_FREE(macID);
+		cpeabStrncpy(deviceMACValue, macID, strlen(macID)+1);
+		macIDToLower(deviceMACValue, deviceMAC);
+		CpeabsDebug("deviceMAC: %s\n",deviceMAC);
+		CPEABS_FREE(macID);
 	}
 	CpeabsDebug("deviceMAC returned from lib is %s\n", deviceMAC);
 	return deviceMAC;
